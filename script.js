1d1e63efcc848528fefb6f3d5a1f199dafd45e1f
John Michael Jarvis <JMJarvis1@icloud.com>
2022-02-17 10:38:12 -0500

    changes

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 "use strict";
 
-for (let i = 0; i < 5; i++) {
-  playRound();
+function game() {
+  let playerCount = 0,
+    computerCount = 0,
+    tieCount = 0,
+    victor;
+
+  for (let i = 0; i < 5; i++) {
+    const winner = playRound();
+    if (winner === "player") {
+      playerCount += 1;
+    } else if (winner === "computer") {
+      computerCount += 1;
+    } else tieCount += 1;
+  }
+  let topScore = Math.max(playerCount, computerCount);
+  if (playerCount === topScore) {
+    victor = "player";
+  } else victor = "computer";
+  if (playerCount != computerCount) {
+    console.log(
+      `The player won ${playerCount} times, the computer won ${computerCount} times, and the constest ended in a tie ${tieCount} times. The ${victor} wins the game!`
+    );
+  } else {
+    console.log(
+      `The player won ${playerCount} times, the computer won ${computerCount} times, and the constest ended in a tie ${tieCount} times. The game ended in a draw!`
+    );
+  }
 }
 
 function promptPlayer() {
   let choice;
   for (let i = 0; i < 3; i++) {
-    choice = prompt(
-      "Choose rock, paper, or scissors to play or click OK to choose randomly:"
-    );
+    choice = prompt("Choose rock, paper, or scissors to play:");
     if (choice === null) {
       i = 3;
     } else if (
@@ -57,30 +80,35 @@ function playRound() {
     );
     if (computerChoice === playerChoice) {
       console.log("Tie!");
+      return "tie";
     } else if (playerChoice === "rock" && computerChoice != "paper") {
       console.log(
         `You win! ${capitalizeFirstLetter(
           playerChoice
         )} beats ${computerChoice}.`
       );
+      return "player";
     } else if (playerChoice === "paper" && computerChoice != "scissors") {
       console.log(
         `You win! ${capitalizeFirstLetter(
           playerChoice
         )} beats ${computerChoice}.`
       );
+      return "player";
     } else if (playerChoice === "scissors" && computerChoice != "rock") {
       console.log(
         `You win! ${capitalizeFirstLetter(
           playerChoice
         )} beats ${computerChoice}.`
       );
+      return "player";
     } else
       console.log(
         `You Lose! ${capitalizeFirstLetter(
           computerChoice
         )} beats ${playerChoice}.`
       );
+    return "computer";
   }
 }
 
